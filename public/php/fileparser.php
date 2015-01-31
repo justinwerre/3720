8c8a8fb345bdda25f8ed3322b13b991c1ca8505a
Josh Enns <joshenns2@gmail.com>
2015-01-30 17:01:40 -0700

    Read .lis and print classes

--- public/php/fileparser.php
+++ public/php/fileparser.php
@@ -1,6 +1,12 @@
 <?php
-	print("post:");
-    var_dump($_POST);
-    print("file:");
-    var_dump($_FILES);
+  error_reporting(E_ALL);
+  $file = fopen("T.lis", "r");
+    while(!feof($file)){
+      $line = fgets($file);
+      $arr = explode(" ", $line);
+      if(ctype_upper($arr[0])){
+        echo $line;
+        echo "<br>";
+      }
+    }
 ?>
