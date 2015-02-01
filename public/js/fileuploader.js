630638a4432dadd534e3b2b368be6b446120b95b
justinwerre <werrejustin@gmail.com>
2015-02-01 16:46:12 -0700

    fixed file uploading

--- public/js/fileuploader.js
+++ public/js/fileuploader.js
@@ -1,24 +1,29 @@
 $(document).ready(function(){
-  $("#filesubmitbutton").on("click",function(){
-    var file_data = $("#fileinput").prop('files')[0];
-    var form_data = new FormData(document.forms.namedItem("testForm"));
-//    form_data.append('file', file_data);
-    console.log(file_data);
-//    var formData = new FormData($("#testForm"));
-//    console.log(file_data);
-    //probably a bad idea
-    try{
-      $.ajax({
-        type: "POST",
-        url: "/php/fileparser.php",
-        data: {filedata: file_data},
-        cache: false,
-        success: function(data, textStatus, jqXHR){
-          console.log(data);
-        }
-      });
-    }catch(e){
-      console.log(e);
-    }
+  $("#filesubmitbutton").on("click", function(){
+    var fileSelect = document.getElementById("fileinput");
+    var files = fileSelect.files;
+    var lis = files[0];
+    var formData = new FormData();
+    formData.append("file", lis, lis.name);
+    
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", "/php/jsdev.php", true);
+    
+    xhr.onload = function(){
+      if(xhr.status === 200){
+        console.log("success", xhr.responseText);
+      }else{
+        console.log("problems");
+      }
+    };
+    
+    $.ajax({
+      type: "POST",
+      url: "/php/jsdev.php",
+      
+    });
+    
+    xhr.send(formData);
+    
   });
 });
