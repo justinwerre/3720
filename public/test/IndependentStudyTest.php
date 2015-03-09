c2b65a4cbdf2ef022a27770ed476559f2bb5cdd4
joshenns <joshenns2@gmail.com>
2015-03-09 15:42:03 -0600

    Added test independent study 2990 course detection - fails

---
+++ public/test/IndependentStudyTest.php
@@ -0,0 +1,21 @@
+<?php
+/*
+ * run phpunit command from public folder
+ * phpunit --bootstrap php/RequirementsChecker.php test/IndependentStudyTest.php
+*/
+require_once "/vagrant/public/php/StudentProfile.php";
+require_once "/vagrant/public/php/Course.php";
+require_once "/vagrant/public/php/RequirementsChecker.php";
+
+class IndependentStudyCheckerTest extends PHPUnit_Framework_TestCase
+{
+  public function test2990()
+  {
+    $student = new StudentProfile();
+    $course = new Course();
+    $course->set("courseNumber",2990);
+    $student->set("courses",$course);
+    $status = checkIndependentStudy($student);
+    $this->assertEquals(true, $status["result"]);
+  }
+}
