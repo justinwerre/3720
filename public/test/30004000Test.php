b50f2c39f06f382b65c2a2296321b05ab5a3e3ff
joshenns <joshenns2@gmail.com>
2015-03-10 09:18:31 -0600

    test 14 math and 1 mgt - fail

--- public/test/30004000Test.php
+++ public/test/30004000Test.php
@@ -123,4 +123,22 @@ class check30004000Test extends PHPUnit_Framework_TestCase
     $status = check30004000Courses($student);
     $this->assertEquals(false, $status["result"]);
   }
+
+  public function testFifteen3000WithMGT()
+  {
+    $student = new StudentProfile();
+    $course = new Course();
+	$course->set("department", "MGT");
+	$course->set("courseNumber", 3000);
+	$student->set("courses", $course);
+    for($i=0;$i<14;$i++)
+    {
+      $course = new Course();
+      $course->set("department", "MATH");
+      $course->set("courseNumber", 3000+$i);
+      $student->set("courses", $course);
+    }
+    $status = check30004000Courses($student);
+    $this->assertEquals(false, $status["result"]);
+  }
 }
