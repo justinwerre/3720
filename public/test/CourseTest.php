26e8c2ed667d7d9226310404e5c4f2785b802338
justinwerre <werrejustin@gmail.com>
2015-03-09 19:52:51 -0600

    switched to relative paths for the require_once in the test files

--- public/test/CourseTest.php
+++ public/test/CourseTest.php
@@ -3,7 +3,7 @@
  * run phpunit command from public folder
  * phpunit --bootstrap php/Course.php test/CourseTest.php
 */
-require_once "/vagrant/public/php/Course.php";
+require_once __DIR__."/../php/Course.php";
 
 class CourseTest extends PHPUnit_Framework_TestCase
 {
