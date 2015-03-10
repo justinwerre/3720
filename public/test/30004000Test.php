e8455d7322990610a7b5bffe56cf248d55c33578
joshenns <joshenns2@gmail.com>
2015-03-10 09:05:30 -0600

    test 14 math and 1 ADCS - fail

--- public/test/30004000Test.php
+++ public/test/30004000Test.php
@@ -33,4 +33,23 @@ class check30004000Test extends PHPUnit_Framework_TestCase
     $status = check30004000Courses($student);
     $this->assertEquals(true, $status["result"]);
   }
+
+  public function testFifteen3000WithADCS()
+  {
+    $student = new StudentProfile();
+    $course = new Course();
+	$course->set("department", "ADCS");
+	$course->set("courseNumber", 3000);
+	$student->set("courses", $course);
+    for($i=0;$i<14;$i++)
+    {
+      $course = new Course();
+      $course->set("department", "MATH");
+      $course->set("courseNumber", 3000+$i);
+      $student->set("courses", $course);
+    }
+    $status = check30004000Courses($student);
+    $this->assertEquals(false, $status["result"]);
+  }
+
 }
