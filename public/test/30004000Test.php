e7a28589406737f00b17e73fa2eb56f6ea451b32
joshenns <joshenns2@gmail.com>
2015-03-10 09:10:02 -0600

    test 14 math and one cdev - fails

--- public/test/30004000Test.php
+++ public/test/30004000Test.php
@@ -52,4 +52,21 @@ class check30004000Test extends PHPUnit_Framework_TestCase
     $this->assertEquals(false, $status["result"]);
   }
 
+  public function testFifteen3000WithCDEV()
+  {
+    $student = new StudentProfile();
+    $course = new Course();
+	$course->set("department", "CDEV");
+	$course->set("courseNumber", 3000);
+	$student->set("courses", $course);
+    for($i=0;$i<14;$i++)
+    {
+      $course = new Course();
+      $course->set("department", "MATH");
+      $course->set("courseNumber", 3000+$i);
+      $student->set("courses", $course);
+    }
+    $status = check30004000Courses($student);
+    $this->assertEquals(false, $status["result"]);
+  }
 }
