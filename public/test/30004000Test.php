8fc7404d7aa8c8f28107553571105425a7819249
joshenns <joshenns2@gmail.com>
2015-03-10 09:33:22 -0600

    test 14 3000 4000 and 1 1000 - pass

--- public/test/30004000Test.php
+++ public/test/30004000Test.php
@@ -226,4 +226,30 @@ class check30004000Test extends PHPUnit_Framework_TestCase
     $status = check30004000Courses($student);
     $this->assertEquals(true, $status["result"]);
   }
+
+  public function testFourteen30004000Plus1000()
+  {
+    $student = new StudentProfile();
+    for($i=0;$i<7;$i++)
+    {
+      $course = new Course();
+      $course->set("department", "MATH");
+      $course->set("courseNumber", 3000+$i);
+      $student->set("courses", $course);
+    }
+    for($i=0;$i<7;$i++)
+    {
+      $course = new Course();
+      $course->set("department", "MATH");
+      $course->set("courseNumber", 4000+$i);
+      $student->set("courses", $course);
+    }
+    $course = new Course();
+    $course->set("department", "MATH");
+    $course->set("courseNumber", 1000);
+    $student->set("courses", $course);
+
+    $status = check30004000Courses($student);
+    $this->assertEquals(false, $status["result"]);
+  }
 }
