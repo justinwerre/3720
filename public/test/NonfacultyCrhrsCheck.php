233a4199482e49b5f3c1d603ac7c4f4c54dc073e
joshenns <joshenns2@gmail.com>
2015-03-11 09:33:09 -0600

    test one of each 2 crhr - pass

--- public/test/NonfacultyCrhrsCheck.php
+++ public/test/NonfacultyCrhrsCheck.php
@@ -220,11 +220,53 @@ class nonfacultyCrhrsTest extends PHPUnit_Framework_TestCase
     $status = checkNonfacultyCrhrs($student);
     $this->assertEquals(false, $status["result"]);
   }
-  // if($department != "ADCS" && $department != "CDEV"&& $department != "CRED"
-  //           && $department != "EDUC" && $department != "HLCS" && $department != "MGT"
-  //           && $department != "NURS" && $department != "PUBH")
-  //       {
-  //         $courses[] = $course->toArray();
-  //       }
+
+  public function testOneOfEachNonfaculty2crhr()
+  {
+    $student = new StudentProfile();
+    $course1 = new Course();
+    $course2 = new Course();
+    $course3 = new Course();
+    $course4 = new Course();
+    $course5 = new Course();
+    $course6 = new Course();
+    $course7 = new Course();
+    $course8 = new Course();
+    $course1->set("department","ADCS");
+    $course1->set("courseNumber",2000);
+    $course1->set("weight",2);
+    $course2->set("department","CDEV");
+    $course2->set("courseNumber",2001);
+    $course2->set("weight",2);
+    $course3->set("department","CRED");
+    $course3->set("courseNumber",2002);
+    $course3->set("weight",2);
+    $course4->set("department","EDUC");
+    $course4->set("courseNumber",2002);
+    $course4->set("weight",2);
+    $course5->set("department","HLCS");
+    $course5->set("courseNumber",2002);
+    $course5->set("weight",2);
+    $course6->set("department","MGT");
+    $course6->set("courseNumber",2002);
+    $course6->set("weight",2);
+    $course7->set("department","NURS");
+    $course7->set("courseNumber",2002);
+    $course7->set("weight",2);
+    $course8->set("department","PUBH");
+    $course8->set("courseNumber",2002);
+    $course8->set("weight",2);
+    $student->set("courses",$course1);
+    $student->set("courses",$course2);
+    $student->set("courses",$course3);
+    $student->set("courses",$course4);
+    $student->set("courses",$course5);
+    $student->set("courses",$course6);
+    $student->set("courses",$course7);
+    $student->set("courses",$course8);
+    $status = checkNonfacultyCrhrs($student);
+    $this->assertEquals(false, $status["result"]);
+  }
+  
 
 }
