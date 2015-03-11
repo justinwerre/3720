3ed39467722e85565bc70a2c03e08b299ecfa55d
cpinter10 <cpinter10@gmail.com>
2015-03-11 00:28:52 -0600

    test applied 2881 to 2885 detect - fails

--- public/test/AppliedStudyTest.php
+++ public/test/AppliedStudyTest.php
@@ -29,4 +29,25 @@ class AppliedStudyCheckerTest extends PHPUnit_Framework_TestCase
     $this->assertEquals(0, $status["result"]);
   }
 
+  public function test2881to2885()
+  {
+    $student = new StudentProfile();
+    $course1 = new Course();
+    $course1->set("courseNumber",2881);
+    $course2 = new Course();
+    $course2->set("courseNumber",2882);
+    $course3 = new Course();
+    $course3->set("courseNumber",2883);
+    $course4 = new Course();
+    $course4->set("courseNumber",2884);
+    $course5 = new Course();
+    $course5->set("courseNumber",2885);
+    $student->set("courses",$course1);
+    $student->set("courses",$course2);
+    $student->set("courses",$course3);
+    $student->set("courses",$course4);
+    $student->set("courses",$course5);
+    $status = checkAppliedStudy($student);
+    $this->assertEquals(5, count($status["reason"]));
+  }
 }
