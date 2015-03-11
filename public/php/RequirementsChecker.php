f49cae0a5773ba742f983fe4b48f6f577c8401ef
joshenns <joshenns2@gmail.com>
2015-03-11 09:10:25 -0600

    test 3 6crhr cred - pass

--- public/php/RequirementsChecker.php
+++ public/php/RequirementsChecker.php
@@ -135,7 +135,7 @@
     foreach($studentProfile->get("courses") as $course)
     {
       $department = $course->get("department");
-      if ($department=="ADCS" || $department=="CDEV")
+      if ($department=="ADCS" || $department=="CDEV" || $department=="CRED")
       {
         $totWeight += $course->get("weight");
       }
