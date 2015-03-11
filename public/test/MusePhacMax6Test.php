f8f43854aae98ad095597f22808e07e5e8c56180
cpinter10 <cpinter10@gmail.com>
2015-03-11 10:44:44 -0600

    test MusePhac max 6 - fails

--- public/test/MusePhacMax6Test.php
+++ public/test/MusePhacMax6Test.php
@@ -28,4 +28,39 @@ class MusePhacMaxCheckerTest extends PHPUnit_Framework_TestCase
     $status = checkMusePhacMax($student);
     $this->assertEquals(1, count($status["reason"]));
   }
+
+  public function testMax6Activity()
+  {
+    $student = new StudentProfile();
+    $course1 = new Course();
+    $course1->set("department","PHAC");
+    $student->set("creditHours", $student->get("creditHours") + 3);
+    $student->set("courses",$course1);
+    $course2 = new Course();
+    $course2->set("department","MUSE");
+    $student->set("creditHours", $student->get("creditHours") + 3);
+    $student->set("courses",$course2);
+    $course3 = new Course();
+    $course3->set("department","PHAC");
+    $student->set("creditHours", $student->get("creditHours") + 3);
+    $student->set("courses",$course3);
+    $course4 = new Course();
+    $course4->set("department","MUSE");
+    $student->set("creditHours", $student->get("creditHours") + 3);
+    $student->set("courses",$course4);
+    $course5 = new Course();
+    $course5->set("department","PHAC");
+    $student->set("creditHours", $student->get("creditHours") + 3);
+    $student->set("courses",$course5);
+    $course6 = new Course();
+    $course6->set("department","MUSE");
+    $student->set("creditHours", $student->get("creditHours") + 3);
+    $student->set("courses",$course6);
+    $course7 = new Course();
+    $course7->set("department","PHAC");
+    $student->set("creditHours", $student->get("creditHours") + 3);
+    $student->set("courses",$course7);
+    $status = checkMusePhacMax($student);
+    $this->assertEquals(18, $student->get("creditHours"));
+  }
 }
