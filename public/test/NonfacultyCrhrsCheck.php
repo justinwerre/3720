a4ec12c1f692b4a74b38560666ab992ba380de03
joshenns <joshenns2@gmail.com>
2015-03-11 08:57:33 -0600

    test three 3chrhr adcs - fail

--- public/test/NonfacultyCrhrsCheck.php
+++ public/test/NonfacultyCrhrsCheck.php
@@ -13,7 +13,8 @@ class nonfacultyCrhrsTest extends PHPUnit_Framework_TestCase
   {
     $student = new StudentProfile();
     $course = new Course();
-    $course->set("department","HDCS");
+    $course->set("department","ADCS");
+    $course->set("courseNumber",2000);
     $course->set("weight",3);
     $student->set("courses",$course);
     $status = checkNonfacultyCrhrs($student);
@@ -24,9 +25,32 @@ class nonfacultyCrhrsTest extends PHPUnit_Framework_TestCase
   {
     $student = new StudentProfile();
     $course = new Course();
-    $course->set("department","HDCS");
+    $course->set("department","ADCS");
+    $course->set("courseNumber",2000);
     $course->set("weight",7);
     $student->set("courses",$course);
+    $status = checkNonfacultyCrhrs($student);
+    $this->assertEquals(false, $status["result"]);
+  }
+
+  public function testThree3CrhrADCS()
+  {
+    $student = new StudentProfile();
+    $course1 = new Course();
+    $course2 = new Course();
+    $course3 = new Course();
+    $course1->set("department","ADCS");
+    $course1->set("courseNumber",2000);
+    $course1->set("weight",3);
+    $course2->set("department","ADCS");
+    $course2->set("courseNumber",2001);
+    $course2->set("weight",3);
+    $course3->set("department","ADCS");
+    $course3->set("courseNumber",2002);
+    $course3->set("weight",3);
+    $student->set("courses",$course1);
+    $student->set("courses",$course2);
+    $student->set("courses",$course3);
     $status = checkNonfacultyCrhrs($student);
     $this->assertEquals(false, $status["result"]);
   }
