e809e8a8a406ec738036568e1bf838cc3786afe5
joshenns <joshenns2@gmail.com>
2015-03-12 18:25:13 -0600

    send array of checks to output

--- public/php/GradCheck.php
+++ public/php/GradCheck.php
@@ -7,16 +7,19 @@
   $filename = $_FILES['file']['tmp_name'];
   $students = array();
   $parsedStudents = array();
+  $checks = array();
   $parsedStudents = parseFile($filename, $students);
   foreach($parsedStudents as $student)
   {
     $requirementChecker = new RequirementsChecker($student);
     $check = $requirementChecker->get();
-    header('Content-Type: application/json');
-    echo json_encode(array(
-    "gradCheck" => $check,
-    "studentProfile" => $student->toArray()
-    ));
+    $checks[] = $check;
+    
   }
+  header('Content-Type: application/json');
+    echo json_encode(array(
+    "gradCheck" => $checks,
+    "studentProfile" => $parsedStudents
+    ));
   
 ?>
