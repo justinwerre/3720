12a39134ad278b0699553d62eb70475ea1874b50
justinwerre <werrejustin@gmail.com>
2015-03-15 17:00:50 -0600

    Added test stats 1770, test fails

--- public/test/EconRequiredCoursesTest.php
+++ public/test/EconRequiredCoursesTest.php
@@ -113,5 +113,17 @@
 			$test = $econCheck->get();
 			$this->assertEquals(1, count($test['requiredCourses']["reason"]));
 		}
+		
+		public function testStats1770()
+		{
+			$student = new StudentProfile();
+			$course = new Course();
+			$course->set("courseNumber", 1770);
+			$course->set("department", "STAT");
+			$student->set("courses",$course);
+			$econCheck = new EconRequirementsChecker($student);
+			$test = $econCheck->get();
+			$this->assertEquals(1, count($test['requiredCourses']["reason"]));
+		}
 	}
 ?>
