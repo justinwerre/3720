18c29054f33c6f6a8e8865636ca2d0f33472ece2
justinwerre <werrejustin@gmail.com>
2015-03-15 16:50:43 -0600

    added test econ 10112, test passed

--- public/test/EconRequiredCoursesTest.php
+++ public/test/EconRequiredCoursesTest.php
@@ -41,5 +41,17 @@
 			$test = $econCheck->get();
 			$this->assertEquals(0, count($test['requiredCourses']["reason"]));
 		}
+		
+		public function testEcon1012()
+		{
+			$student = new StudentProfile();
+			$course = new Course();
+			$course->set("courseNumber",1012);
+			$course->set("department", "ECON");
+			$student->set("courses",$course);
+			$econCheck = new EconRequirementsChecker($student);
+			$test = $econCheck->get();
+			$this->assertEquals(1, count($test['requiredCourses']["reason"]));
+		}
 	}
 ?>
