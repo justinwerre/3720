54bf72b3b7f211f5216228f880cfac7369288467
justinwerre <werrejustin@gmail.com>
2015-03-15 16:59:03 -0600

    added test 3950, test passes

--- public/test/EconRequiredCoursesTest.php
+++ public/test/EconRequiredCoursesTest.php
@@ -101,5 +101,17 @@
 			$test = $econCheck->get();
 			$this->assertEquals(1, count($test['requiredCourses']["reason"]));
 		}
+		
+		public function testEcon3950()
+		{
+			$student = new StudentProfile();
+			$course = new Course();
+			$course->set("courseNumber", 3950);
+			$course->set("department", "ECON");
+			$student->set("courses",$course);
+			$econCheck = new EconRequirementsChecker($student);
+			$test = $econCheck->get();
+			$this->assertEquals(1, count($test['requiredCourses']["reason"]));
+		}
 	}
 ?>
