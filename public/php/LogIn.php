01445a9336e35ee4137ef004a04ea1e1a1832a67
Tim <T-Unit@Ts-Mac-Pro.local>
2015-03-15 13:57:00 -0600

    Add Login script
    LogIn.php to /php

---
+++ public/php/LogIn.php
@@ -0,0 +1,32 @@
+<?php
+session_start();
+
+$username=$_POST["username"];
+$password=$_POST["password"];
+
+//Should be using hash_equals, doesn't come up
+$file = fopen("credentials.txt", "r")
+
+while(!feof($file))
+{
+    $line = fgets($file);
+    $arr = explode(" ", $line);
+
+    var_dump($arr);
+
+    $salt = 1234;
+
+    if($storedUser == $username && $storedPass == crypt($username, $storedSalt))
+    {
+        $_SESSION['username']=$username;
+        header("/html/checker.html");
+    }
+    else 
+    {
+        echo "Wrong Username or Password, redirecting in 3 seconds..."
+        header("refresh:3;url:index.html");
+    }
+}
+
+fclose($file);
+?>
