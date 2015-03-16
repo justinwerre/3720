07e62fa3126e76a2ad45a395e388324f10c7b590
Adam-3720 <abhijeet.shende@uleth.ca>
2015-03-16 16:03:36 -0600

    testIndStudy>5pass

--- public/php/RequirementsChecker.php
+++ public/php/RequirementsChecker.php
@@ -82,7 +82,7 @@
 
   	return array
   	(
-      "result" => count($independentStudyCourses)<=6,
+      "result" => count($independentStudyCourses)<=5,
       "reason" => $independentStudyCourses
     );
   }
